d84e6af6cc480e7fffedbb0b06128ddcfb70ee64
wilson <mutidawilz@gmail.com>
2025-02-23 10:16:03 +0300

    Forms

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,15 +2,19 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 import relationship_app.views as views  # Import views explicitly
 
-import relationship_app.views as views
-
 urlpatterns = [
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', views.register, name='register'),  # Explicitly reference views.register
+    path('register/', views.register, name='register'),
+    
+    # Book-related URLs
     path('books/', views.list_books, name='list_books'),
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
+    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
+
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
