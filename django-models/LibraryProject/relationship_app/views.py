d2b538a247a26821932aea299ec7e2a62a2f6c83
wilson <mutidawilz@gmail.com>
2025-02-23 08:29:10 +0300

    Added all files in templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,7 @@ from .models import Author, Book, Librarian, Library
 
 def list_all_books(request):
     books = Book.objects.all()
-    return render(request, "relationship_app/   list_books.html", {'books': books})
+    return render(request, "relationship_app/list_books.html", {'books': books})
 
 
 class LibraryDetailView(DetailView):
