f7646bc952892176befe60ea61ac3becec5790b5
wilson <mutidawilz@gmail.com>
2025-02-23 08:36:54 +0300

    Added all files in templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import DetailView
 
-from .models import Author, Book, Librarian, Library
+from .models import Library
+
+from .models import Author, Book, Librarian
 
 def list_all_books(request):
     books = Book.objects.all()
