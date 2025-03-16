928fd5181c1be0cd19d68300f2ed112ff406bf2e
wilson <mutidawilz@gmail.com>
2025-03-16 21:47:38 +0300

    Added third task

---
+++ advanced-api-project/api/test_views.py
@@ -0,0 +1,66 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth.models import User
+from .models import Book, Author
+from .serializers import BookSerializer
+
+class BookAPITestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.author = Author.objects.create(name='Test Author')
+        self.book1 = Book.objects.create(title='Test Book 1', publication_year=2023, author=self.author)
+        self.book2 = Book.objects.create(title='Test Book 2', publication_year=2022, author=self.author)
+
+    def test_list_books(self):
+        response = self.client.get('/api/books/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_create_book_authenticated(self):
+        self.client.force_authenticate(user=self.user)
+        data = {'title': 'New Book', 'publication_year': 2024, 'author': self.author.id}
+        response = self.client.post('/api/books/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.count(), 3)
+
+    def test_create_book_unauthenticated(self):
+        data = {'title': 'New Book', 'publication_year': 2024, 'author': self.author.id}
+        response = self.client.post('/api/books/', data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_retrieve_book(self):
+        response = self.client.get(f'/api/books/{self.book1.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Test Book 1')
+
+    def test_update_book_authenticated(self):
+        self.client.force_authenticate(user=self.user)
+        data = {'title': 'Updated Book', 'publication_year': 2023, 'author': self.author.id}
+        response = self.client.put(f'/api/books/{self.book1.id}/', data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.book1.refresh_from_db()
+        self.assertEqual(self.book1.title, 'Updated Book')
+
+    def test_delete_book_authenticated(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(f'/api/books/{self.book1.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Book.objects.count(), 1)
+
+    def test_filter_books(self):
+        response = self.client.get('/api/books/?publication_year=2023')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['title'], 'Test Book 1')
+
+    def test_search_books(self):
+        response = self.client.get('/api/books/?search=Test%20Book')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_order_books(self):
+        response = self.client.get('/api/books/?ordering=-publication_year')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]['title'], 'Test Book 1')
+        self.assertEqual(response.data[1]['title'], 'Test Book 2')
