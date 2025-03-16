7cd4683c4fb85fc6e7ed13e0d4f77deebfe2ac3d
wilson <mutidawilz@gmail.com>
2025-03-16 20:09:45 +0300

    Initial Project setup

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 
+# Author model
 class Author(models.Model):
-    """Model representing an author."""
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
+# book model
 class Book(models.Model):
-    """Model representing a book, linked to an author."""
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=100)
     publication_year = models.IntegerField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
-        return self.title
-
+        return f'{self.title} by ({self.author.name})'
--- advanced_api_project/api/serializers.py
+++ advanced_api_project/api/serializers.py
@@ -1,23 +1,21 @@
 from rest_framework import serializers
-from .models import Author, Book
-import datetime
+from .models import Book, Author
 
+# Serializer for the book model
 class BookSerializer(serializers.ModelSerializer):
-    """Serializer for Book model, with validation to prevent future years."""
-    
+    publication_year = serializers.IntegerField()
+
     class Meta:
         model = Book
         fields = ['id', 'title', 'publication_year', 'author']
 
-    def validate_publication_year(self, value):
-        """Ensure publication year is not in the future."""
-        current_year = datetime.date.today().year
-        if value > current_year:
-            raise serializers.ValidationError("Publication year cannot be in the future.")
-        return value
+    def validate_publication_year(self, year):
+        if year > 2025:
+            raise serializers.ValidationError("Publication year is not in the future!")
+        return year
 
+# serializer for the author model
 class AuthorSerializer(serializers.ModelSerializer):
-    """Serializer for Author model, including nested books."""
     books = BookSerializer(many=True, read_only=True)
 
     class Meta:
