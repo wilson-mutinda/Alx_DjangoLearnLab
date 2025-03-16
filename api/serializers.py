aaaae841cc901f2d0e26d01523258d6b52cf4fd1
wilson <mutidawilz@gmail.com>
2025-03-16 07:23:48 +0300

    Initial Commit

---
+++ api/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Book, Author
+
+
+# Serializer for the book model
+class BookSerializer(serializers.ModelSerializer):
+
+    publication_year = serializers.IntegerField()
+    class Meta:
+        model = Book
+        fields = ['id','title', 'publication_year', 'author']
+
+    def validate_publication_year(self, year):
+        # validate the year, that it cannot be in future!
+        if year > 2025:
+            raise serializers.ValidationError("Publication year is not in the future!")
+        return year
+
+# serializer for the author model   
+class AuthorSerializer(serializers.ModelSerializer):
+
+    books = BookSerializer(many = True, read_only = True)
+
+    class Meta:
+        model = Author
+        fields = ['id', 'name', 'books']
