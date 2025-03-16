c87a52d5ba95d3b89b739fc94ccd042a1ea81fa5
wilson <mutidawilz@gmail.com>
2025-03-16 20:39:01 +0300

    initial commit with changes

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return f'{self.title} by ({self.author.name})'
---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Book, Author
+
+class BookSerializer(serializers.ModelSerializer):
+    publication_year = serializers.IntegerField()
+
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'publication_year', 'author']
+
+    def validate_publication_year(self, year):
+        if year > 2025:
+            raise serializers.ValidationError("Publication year is not in the future!")
+        return year
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Author
+        fields = ['id', 'name', 'books']
