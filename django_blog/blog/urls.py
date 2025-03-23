aee5f018ae350d598f86f5d2925f075ea7a13a0e
wilson <mutidawilz@gmail.com>
2025-03-23 16:29:46 +0300

    added the task 2

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile, name='profile'),
+
+    path('list/', views.PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/create/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/',views.PostUpdateView.as_view(), name='post-edit'),
+    path('post/<int:pk>/delete/', views.DeleteView.as_view(), name='post-delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -5,6 +5,12 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from .forms import PostForm
+from django.urls import reverse_lazy
+
 # Custom registration form
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField()
@@ -57,3 +63,45 @@ def edit_profile(request):
         request.user.save()
         return redirect('profile')
     return render(request, 'blog/edit_profile.html')
+
+# List all posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+# show a single post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+# create a new post
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+# Update an existing post
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+    
+# Delete a post
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+
+    def  test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
