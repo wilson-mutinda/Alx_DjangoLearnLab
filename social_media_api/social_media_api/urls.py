42907f0b477663d37f09fd3b6886ba7113a6ac11
wilson <mutidawilz@gmail.com>
2025-03-24 06:30:28 +0300

    Added the second app

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('posts/', include('posts.urls')),
+    path('api/', include('posts.urls')),
 ]
