86a7f305cf97e2ee1c4a2978364dd4dc873be749
wilson <mutidawilz@gmail.com>
2025-03-30 17:00:32 +0300

    Added the correction for task 4

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -3,8 +3,8 @@ from .views import PostViewSet, CommentViewSet, LikeViewSet, UserFeedView
 
 urlpatterns = [
     path('posts/', PostViewSet.as_view({'get': 'list', 'post': 'create'}), name='posts'),
-    path('posts/<int:post_id>/like/', LikeViewSet.as_view({'post': 'create'}), name='like-post'),
-    path('posts/<int:post_id>/unlike/', LikeViewSet.as_view({'delete': 'destroy'}), name='unlike-post'),
+    path('posts/<int:pk>/like/', LikeViewSet.as_view({'post': 'create'}), name='like-post'),
+    path('posts/<int:pk>/unlike/', LikeViewSet.as_view({'delete': 'destroy'}), name='unlike-post'),
     path('comments/', CommentViewSet.as_view({'get': 'list', 'post': 'create'}), name='comments'),
     path('feed/', UserFeedView.as_view(), name='user-feed'),
 ]
